9be63ad46542a0d54d60ff6aab4d7368830f6c23
PatriciaGB18 <pbarros@byupathway.edu>
2025-03-04 19:22:58 -0300

    guess the number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes"; 
+
+        while (playAgain.ToLower() == "yes") 
+        {
+            Random rand = new Random();
+            int magicNumber = rand.Next(1, 101); 
+            int guess = -1; 
+            int guessCount = 0; 
+            
+
+            Console.WriteLine("I have selected a magic number between 1 and 100. Try to guess it!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine(); 
+
+                if (int.TryParse(input, out guess)) 
+                {
+                    guessCount++;
+
+                    if (guess == magicNumber)
+                    {
+                        Console.WriteLine("You are correct!");
+                        Console.WriteLine("It took you " + guessCount + " guesses.");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
